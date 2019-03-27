6dfc1251e5b0ae2f7164527be0352cfabf85e0b9
Tim McMackin <timothymcmackin@users.noreply.github.com>
2019-03-27 10:40:09 -0400

    DEVPLAT-302: Provide custom examples for the SDK (#7)
    * use templates to generate SDK
    
    * better location for templates
    
    * customize templates for better docs
    
    * need to use Docker for now
    
    * use templates to generate SDK
    
    * customize templates for better docs
    
    * updated openapi with examples for SDK
    
    * filter out examples, schemas, and endpoints that dont belong in SDK
    
    * these files are in build/sdk/js/templates now
    
    * lost this dependency
    
    * updates to openapi file including examples with new auth shortcut
    
    * apiclient var not needed for oauth2
    
    * lint examples
    
    * format of generated examples
    
    * example updates
    
    * need this apiClient object for basic auth
    
    * don't need a separate example for JS and SDK
    
    * using the SDK for all JS examples now

--- build/swagger/openapiToSwagger.js
+++ build/swagger/openapiToSwagger.js
@@ -1,16 +1,92 @@
 const { promisify } = require('util');
 const { convert } = require('api-spec-converter');
 const fs = require('fs');
+const _ = require('lodash');
 
 const openapi = require('../../openapi');
 
 const convertPromise = promisify(convert);
 
-convertPromise({
-  from: 'openapi_3',
-  to: 'swagger_2',
-  source: openapi,
-}).then(({ spec }) => {
-  fs.writeFileSync('swagger2.json', JSON.stringify(spec, null, 2))
+const languagesToInclude = ["javascript--nodejs"];
+const endpointsToFilterOutRegex = /\/v2\/oauth\/.*/;
+const schemasToFilter = ["OauthAccessTokenResponse", "AuthorizeResponse"];
+
+_.mixin({
+  filterEndpoints,
+  filterSchemas,
+  filterExamples,
+});
+
+async function filterEndpoints(specPromise, endpointsToFilterOutRegex) {
+  const spec = await Promise.resolve(specPromise);
+  if (endpointsToFilterOutRegex) {
+    spec.paths = _.reduce(spec.paths, (acc, value, key, collection) => {
+      if (!key.match(endpointsToFilterOutRegex)) {
+        acc[key] = value;
+      }
+      return acc;
+    }, {})
+  }
+  return spec;
+}
+
+async function filterSchemas(specPromise, schemasToFilter) {
+  const spec = await Promise.resolve(specPromise);
+  if (schemasToFilter) {
+    spec.components.schemas = _.reduce(spec.components.schemas, (acc, value, key, collection) => {
+      if (!schemasToFilter.includes(key)) {
+        acc[key] = value;
+      }
+      return acc;
+    }, {})
+  }
+  return spec;
+}
+
+async function filterExamples(specPromise, languagesToInclude) {
+  const spec = await Promise.resolve(specPromise);
+  if (languagesToInclude) {
+    return recursiveFilter(spec, languagesToInclude);
+  } else {
+    return spec;
+  }
+}
+
+function recursiveFilter(specObject, languagesToInclude) {
+
+  if (_.isArray(specObject)) {
+    return specObject;
+  }
+
+  return(_.mapValues(specObject, (value, key, object) => {
+    if (key === "x-code-samples") {
+      return value.filter(lang => {
+        return languagesToInclude.includes(lang.lang);
+      });
+    }
+    if (!_.isObject(value)){
+      return value;
+    } else {
+      return recursiveFilter(value, languagesToInclude);
+    }
+    
+  }))
+}
+
+const filteredOpenapi = _.chain(openapi)
+  .filterEndpoints(endpointsToFilterOutRegex)
+  .filterSchemas(schemasToFilter)
+  .filterExamples(languagesToInclude)
+  .value();
+
+filteredOpenapi.then(filteredOpenapiData => {
+  convertPromise({
+    from: 'openapi_3',
+    to: 'swagger_2',
+    source: filteredOpenapiData,
+  })
+  .then(({ spec }) => {
+    fs.writeFileSync('swagger2.json', JSON.stringify(spec, null, 2))
+  });
 });
 
