61a571d5d1c9e5989bc9e466b7b3939e2ae13f02
Tim McMackin <tmcmackin@shutterstock.com>
2019-05-01 08:55:04 -0400

    these files are not needed

--- build/jekyll/js/_site/test.js
+++
@@ -1,6 +0,0 @@
-var testString = "A block corresponds to a purchase of one or more allotments. Often, a blockthing results thingblock in a single allotment, su";
-const matchString = '\\b([\\w-]*' + 'block' + '[\\w-]*)\\b';
-const matchRegex = new RegExp(matchString, 'g');
-var matches = testString.match(matchRegex);
-
-console.log(matches);
